ea66eb892c87cb5c4e16f53213d9cc5b007c42da
Axel Zango Copons <68034589+azc-wm@users.noreply.github.com>
2023-10-29 20:10:46 +0000

    treure autorebuild

--- medusa-config.js
+++ medusa-config.js
@@ -45,9 +45,9 @@ const plugins = [
   {
     resolve: "@medusajs/admin",
     /** @type {import('@medusajs/admin').PluginOptions} */
-    options: {
-      autoRebuild: true,
-    },
+    // options: {
+    //   autoRebuild: true,
+    // },
   },
 ];
 
